334ea8ef22cac01af255f45c68a14f6aa077e13d
Yonnas Getahun <yonnastgetahun@gmail.com>
2025-07-16 08:51:56 -0700

    Add GTM analytics

--- app/layout.js
+++ app/layout.js
@@ -42,7 +42,7 @@ export const metadata = {
     card: "summary_large_image",
     title: "Meeting Cost Calculator - What Are Meetings Really Costing?",
     description: "Calculate the true cost of meetings including hidden productivity losses",
-    site: "@pulsecollabagency", // Add your Twitter handle if you have one
+    site: "@pulsecollabagency",
   },
   robots: {
     index: true,
@@ -68,6 +68,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager Script */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MP7JXQLM');
+            `,
+          }}
+        />
+        
         <noscript>
           <img 
             height="1" 
@@ -78,6 +93,16 @@ export default function RootLayout({ children }) {
         </noscript>
       </head>
       <body className={`${inter.className} antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MP7JXQLM"
+            height="0" 
+            width="0" 
+            style={{display: 'none', visibility: 'hidden'}}
+          />
+        </noscript>
+        
         {/* Meta Pixel Code using Next.js Script component */}
         <Script
           id="meta-pixel"
@@ -124,6 +149,13 @@ export default function RootLayout({ children }) {
                 }
                 if (window.fbq) {
                   window.fbq('trackCustom', eventName, params);
+                }
+                // Also push to GTM dataLayer
+                if (window.dataLayer) {
+                  window.dataLayer.push({
+                    'event': eventName,
+                    ...params
+                  });
                 }
               };
               
